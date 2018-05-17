829adc05fa6a907bb26dd0949c5bb7ebe1c74968
Alexandr Semenov <darkrain7@mail.ru>
2018-05-17 23:22:53 +0700

    123

--- src/main/java/test/HomeworkApplication.java
+++ src/main/java/test/HomeworkApplication.java
@@ -3,6 +3,7 @@ package test;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * Created by User on 17.05.2018.
@@ -10,6 +11,7 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
 @EntityScan(value = "test.home.*")
+@ComponentScan("test.home.*")
 public class HomeworkApplication {
     public static void main(String[] args) {
         SpringApplication.run(HomeworkApplication.class, args);
